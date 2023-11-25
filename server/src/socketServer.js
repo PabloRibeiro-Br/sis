e6457c9a864f18ba1db83402a88dc06d6723e22a
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-25 19:03:13 -0300

    ddsa

--- server/src/socketServer.js
+++ server/src/socketServer.js
@@ -77,7 +77,7 @@ const conversationMessageHandler = async (socket, data) => {
       prompt: "Responder em Português do Brasil." + "Você é uma assistente para mecânicos de automóveis." 
               + "Procedimentos de Reparação." + "Diagnóstico pelo defeito."+ "Códigos de Falhas DTC."  + message.content,
                temperature: 0.1,
-               max_tokens: 120,
+               max_tokens: 320,
                top_p: 0.1,
     });
 
