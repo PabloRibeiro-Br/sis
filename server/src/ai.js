2eed2defb16ba2de6105ce044f53d50eeebd74fa
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-25 17:07:41 -0300

    sd

--- server/src/ai.js
+++ server/src/ai.js
@@ -5,7 +5,7 @@ let openai;
 
 const connectWithOpenAIApi = () => {
   const configuration = new Configuration({
-    /* organization: process.env.OPEN_AI_ORG, */
+    organization: process.env.OPEN_AI_ORG,
     apiKey: process.env.OPEN_AI_API_KEY,
   });
 
