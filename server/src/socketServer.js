b4449cab1f7d9094aedfffe569167faba67d41a3
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-25 21:52:42 -0300

    red3

--- server/src/socketServer.js
+++ server/src/socketServer.js
@@ -57,7 +57,7 @@ const conversationMessageHandler = async (socket, data) => {
 
   const openai = getOpenai();
 
-  const previousConversationMessages = [];
+  let conversationHistory = [];
 
   if (sessions[sessionId]) {
     const existingConversation = sessions[sessionId].find(
@@ -65,25 +65,28 @@ const conversationMessageHandler = async (socket, data) => {
     );
 
     if (existingConversation) {
-      previousConversationMessages.push(
-        ...existingConversation.messages.map((m) => ({
-          content: m.content,
-          role: m.aiMessage ? "assistant" : "user",
-        }))
-      );
+      conversationHistory = existingConversation.messages.map((m) => ({
+        content: m.content,
+        role: m.aiMessage ? "assistant" : "user",
+      }));
     }
+
+    const prompt = conversationHistory.map(m => 
+      `${m.role === 'user' ? 'User' : 'Assistant'}: ${m.content}`
+    ).join('\n');
+
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "Você é um Mecânico de Automóveis, Especializado em Injeção Eletrônica e Motores a Mais de 28 anos, Fale como um especialista no assunto. Você responde para profissionais de reparação de automóveis, não para donos de véiculos. Seja Objetivo, fale como um professor de mecânica." + message.content,
-               temperature: 0.1,
-               max_tokens: 200,  
-               top_p:0.8,
-               presence_penalty:0.8,
-               frequency_penalty:0.8,
-               n:2, 
-        });
+      prompt: prompt,
+      temperature: 0.1,
+      max_tokens: 200,  
+      top_p:0.8,
+      presence_penalty:0.8,
+      frequency_penalty:0.8,
+      n:2, 
+    });
 
-   const aiMessageContent = response?.data?.choices[0]?.text;
+    const aiMessageContent = response?.data?.choices[0]?.text;
 
     const aiMessage = {
       content: aiMessageContent
@@ -113,14 +116,6 @@ const conversationMessageHandler = async (socket, data) => {
     );
 
     socket.emit("conversation-details", updatedConversation);
-  }
-};
-
-const conversationDeleteHandler = (_, data) => {
-  const { sessionId } = data;
-
-  if (sessions[sessionId]) {
-    sessions[sessionId] = [];
   }
 };
 
