3b053cf286d406ee2cec57b123efb9f57e865f0f
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-25 23:48:24 -0300

    fsaaa

--- chatgpt/src/Dashboard/dashboardSlice.js
+++ chatgpt/src/Dashboard/dashboardSlice.js
@@ -1,13 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import fs from 'fs'; // Módulo para manipulação de arquivos (Node.js)
 
-const loadConversationsFromLocalStorage = () => {
-  const savedConversations = localStorage.getItem('conversations');
-  return savedConversations ? JSON.parse(savedConversations) : [];
+// Função utilitária para carregar conversações do arquivo JSON
+const loadConversationsFromFile = () => {
+  try {
+    const data = fs.readFileSync('conversations.json');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Erro ao carregar conversações do arquivo JSON', error);
+    return [];
+  }
+};
+
+// Função utilitária para salvar conversações no arquivo JSON
+const saveConversationsToFile = (conversations) => {
+  try {
+    fs.writeFileSync('conversations.json', JSON.stringify(conversations));
+  } catch (error) {
+    console.error('Erro ao salvar conversações no arquivo JSON', error);
+  }
 };
 
 const initialState = {
   sessionEstablished: false,
-  conversations: loadConversationsFromLocalStorage(),
+  conversations: loadConversationsFromFile(),
   selectedConversationId: null,
 };
 
@@ -34,12 +50,15 @@ const dashboardSlice = createSlice({
         });
       }
 
-      // Atualizando o localStorage
-      localStorage.setItem('conversations', JSON.stringify(state.conversations));
+      // Atualizando o arquivo JSON
+      saveConversationsToFile(state.conversations);
     },
     setConversations: (state, action) => {
       state.conversations = action.payload;
       state.sessionEstablished = true;
+
+      // Salvando no arquivo JSON
+      saveConversationsToFile(state.conversations);
     },
     setConversationHistory: (state, action) => {
       const { id, messages } = action.payload;
@@ -54,10 +73,16 @@ const dashboardSlice = createSlice({
           messages,
         });
       }
+
+      // Atualizando o arquivo JSON
+      saveConversationsToFile(state.conversations);
     },
     deleteConversations: (state) => {
       state.conversations = [];
       state.selectedConversationId = null;
+
+      // Salvando no arquivo JSON
+      saveConversationsToFile(state.conversations);
     },
   },
 });
