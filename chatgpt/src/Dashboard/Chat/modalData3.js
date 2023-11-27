9f6af8ac99a53a98656e7d8d9e3640b644a3a2b5
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-27 12:08:02 -0300

    fsa

---
+++ chatgpt/src/Dashboard/Chat/modalData3.js
@@ -0,0 +1,51 @@
+const modalItems = [
+    { 
+        id: 1, 
+        title: "Lubrificante do Motor", 
+        description: "Recomendações para a escolha adequada do lubrificante de acordo com as especificações do fabricante."
+    },
+    { 
+        id: 2, 
+        title: "Injeção Eletrônica", 
+        description: "Principais procedimentos de diagnóstico e solução de problemas no sistema de injeção eletrônica de veículos automotivos."
+    },
+    { 
+        id: 3, 
+        title: "Sistema de Ignição", 
+        description: "Abordagem técnica para diagnosticar e corrigir falhas no sistema de ignição, incluindo testes de componentes-chave."
+    },
+    { 
+        id: 4, 
+        title: "Filtro de Combustível", 
+        description: "Indicações sobre a escolha e substituição adequada do filtro de combustível durante os procedimentos de manutenção."
+    },
+    { 
+        id: 5, 
+        title: "Sistema de Arrefecimento", 
+        description: "Princípios de funcionamento e procedimentos de manutenção do sistema de arrefecimento do motor."
+    },
+    { 
+        id: 6, 
+        title: "Transmissão Automática", 
+        description: "Diagnóstico e solução de problemas comuns em transmissões automáticas, incluindo verificações de fluido e ajustes."
+    },
+    { 
+        id: 7, 
+        title: "Sistema de Freios", 
+        description: "Técnicas de inspeção e reparo do sistema de freios, incluindo substituição de pastilhas, discos e sangramento do sistema."
+    },
+    { 
+        id: 8, 
+        title: "Suspensão", 
+        description: "Práticas recomendadas para diagnóstico e reparo de componentes do sistema de suspensão, como amortecedores e molas."
+    },
+    { 
+        id: 9, 
+        title: "Sistema Elétrico", 
+        description: "Procedimentos de diagnóstico elétrico, incluindo testes de bateria, alternador e sistema de partida."
+    }
+];
+
+export default modalItems;
+
+  
