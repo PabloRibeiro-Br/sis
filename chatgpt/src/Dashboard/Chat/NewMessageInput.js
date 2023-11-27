840641989d55adaef0ad82cad147a3d89ee184f0
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-27 15:52:42 -0300

    edde

--- chatgpt/src/Dashboard/Chat/NewMessageInput.js
+++ chatgpt/src/Dashboard/Chat/NewMessageInput.js
@@ -15,141 +15,141 @@ const NewMessageInput = () => {
  const dispatch = useDispatch();
 
  const selectedConversationId = useSelector(
-  (state) => state.dashboard.selectedConversationId
+ (state) => state.dashboard.selectedConversationId
  );
 
  const conversations = useSelector((state) => state.dashboard.conversations);
 
  const selectedConversation = conversations.find(
-  (c) => c.id === selectedConversationId
+ (c) => c.id === selectedConversationId
  );
 
  const proceedMessage = (text) => {
-  const message = {
-    aiMessage: false,
-    content: text,
-    id: uuid(),
-    animate: false,
-  };
+ const message = {
+   aiMessage: false,
+   content: text,
+   id: uuid(),
+   animate: false,
+ };
 
-  const conversationId =
-    selectedConversationId === "new" ? uuid() : selectedConversationId;
+ const conversationId =
+   selectedConversationId === "new" ? uuid() : selectedConversationId;
 
-  dispatch(
-    addMessage({
-      conversationId,
-      message,
-    })
-  );
+ dispatch(
+   addMessage({
+     conversationId,
+     message,
+   })
+ );
 
-  dispatch(setSelectedConversationId(conversationId));
+ dispatch(setSelectedConversationId(conversationId));
 
-  sendConversationMessage(message, conversationId);
+ sendConversationMessage(message, conversationId);
 
-  setContent("");
+ setContent("");
  };
 
  const handleSendMessage = () => {
-  if (content.length > 0) {
-    proceedMessage(content);
-  }
+ if (content.length > 0) {
+   proceedMessage(content);
+ }
  };
 
  const handleKeyPressed = (event) => {
-  if (event.code === "Enter" && content.length > 0) {
-    proceedMessage(content);
-  }
+ if (event.code === "Enter" && content.length > 0) {
+   proceedMessage(content);
+ }
  };
 
  const openModal = () => {
-  setShowModal(true);
+ setShowModal(true);
  };
 
  const closeModal = () => {
-  setShowModal(false);
+ setShowModal(false);
  };
 
  const handleItemClick = (item) => {
-  closeModal();
+ closeModal();
 
-  if (item && item.description) {
-    const combinedContent = `${content} ${item.description}`.trim();
-    setContent(combinedContent);
+ if (item && item.description) {
+   const combinedContent = `${content} ${item.description}`.trim();
+   setContent(combinedContent);
 
-    if (selectedButton === 5) {
-      proceedMessage(combinedContent);
-    }
-  }
+   if (selectedButton === 5) {
+     proceedMessage(combinedContent);
+   }
+ }
  };
 
  const handleButtonClick = (buttonId) => {
-  setSelectedButton(buttonId);
+ setSelectedButton(buttonId);
  };
 
  const extraButtons = [
-  { id: 1, label: "Botão 1", modalData: require("./modalData").default },
-  { id: 2, label: "Botão 2", modalData: require("./modalData1").default },
-  { id: 3, label: "Botão 3", modalData: require("./modalData2").default },
-  { id: 4, label: "Botão 4", modalData: require("./modalData1").default },
-  { id: 5, label: "Botão 5", modalData: require("./modalData3").default },
+ { id: 1, label: "Botão 1", modalData: require("./modalData").default },
+ { id: 2, label: "Botão 2", modalData: require("./modalData1").default },
+ { id: 3, label: "Botão 3", modalData: require("./modalData2").default },
+ { id: 4, label: "Botão 4", modalData: require("./modalData1").default },
+ { id: 5, label: "Botão 5", modalData: require("./modalData3").default },
  ];
 
  const openExtraModal = (modalData) => {
-  setShowModal(true);
-  setModalData(modalData);
+ setShowModal(true);
+ setModalData(modalData);
  };
 
  return (
-  <div className="new-message-input-container">
-      <button className="send-button" onClick={handleSendMessage}>
-        <BsSend />
-      </button>
-    <input
-      className="new_message_input"
-      placeholder="Enviar mensagem..."
-      value={content}
-      onChange={(e) => setContent(e.target.value)}
-      onKeyDown={handleKeyPressed}
-      disabled={
-        selectedConversation &&
-        !selectedConversation.messages[
-          selectedConversation.messages.length - 1
-        ].aiMessage
-      } 
-    />
-
-    <div className="button-container">
-    
-      {extraButtons.map((button) => (
-        <button 
-          key={button.id}
-          className="open-modal-button"
-          onClick={() => {
-            openExtraModal(button.modalData);
-            handleButtonClick(button.id);
-          }}
-        >
-          {button.label}
-        </button>
-      ))}
-    </div>
-    {showModal && (
-      <div className="modal">
-        <div className="modal-content">
-          <span className="close" onClick={closeModal}>
-            &times;
-          </span>
-          <ul>
-            {modalData && modalData.map((item) => (
-              <li key={item.id} onClick={() => handleItemClick(item)}>
-               {item.title}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    )}
-  </div>
+ <div className="new-message-input-container">
+   <div className="input-button-container">
+     <input
+       className="new_message_input"
+       placeholder="Enviar mensagem..."
+       value={content}
+       onChange={(e) => setContent(e.target.value)}
+       onKeyDown={handleKeyPressed}
+       disabled={
+         selectedConversation &&
+         !selectedConversation.messages[
+           selectedConversation.messages.length - 1
+         ].aiMessage
+       } 
+     />
+     <button className="send-button" onClick={handleSendMessage}>
+       <BsSend />
+     </button>
+   </div>
+   <div className="button-container">
+     {extraButtons.map((button) => (
+       <button 
+         key={button.id}
+         className="open-modal-button"
+         onClick={() => {
+           openExtraModal(button.modalData);
+           handleButtonClick(button.id);
+         }}
+       >
+         {button.label}
+       </button>
+     ))}
+   </div>
+   {showModal && (
+     <div className="modal">
+       <div className="modal-content">
+         <span className="close" onClick={closeModal}>
+           &times;
+         </span>
+         <ul>
+           {modalData && modalData.map((item) => (
+             <li key={item.id} onClick={() => handleItemClick(item)}>
+              {item.title}
+             </li>
+           ))}
+         </ul>
+       </div>
+     </div>
+   )}
+ </div>
  );
 };
 
