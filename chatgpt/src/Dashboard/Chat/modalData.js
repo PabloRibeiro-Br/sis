91d9a25a6b9e4a6f56d22aac4df4691c3efa8854
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-27 23:10:22 -0300

    ogh

--- chatgpt/src/Dashboard/Chat/modalData.js
+++ chatgpt/src/Dashboard/Chat/modalData.js
@@ -1,49 +1,36 @@
 const modalItems = [
-    { 
-        id: 1, 
-        title: "Agile 1.4", 
-        description: "Chevrolet Agile 1.4, "
-    },
-    { 
-        id: 2, 
-        title: "Injeção Eletrônica", 
-        description: "Principais procedimentos de diagnóstico e solução de problemas no sistema de injeção eletrônica de veículos automotivos."
-    },
-    { 
-        id: 3, 
-        title: "Sistema de Ignição", 
-        description: "Abordagem técnica para diagnosticar e corrigir falhas no sistema de ignição, incluindo testes de componentes-chave."
-    },
-    { 
-        id: 4, 
-        title: "Filtro de Combustível", 
-        description: "Indicações sobre a escolha e substituição adequada do filtro de combustível durante os procedimentos de manutenção."
-    },
-    { 
-        id: 5, 
-        title: "Sistema de Arrefecimento", 
-        description: "Princípios de funcionamento e procedimentos de manutenção do sistema de arrefecimento do motor."
-    },
-    { 
-        id: 6, 
-        title: "Transmissão Automática", 
-        description: "Diagnóstico e solução de problemas comuns em transmissões automáticas, incluindo verificações de fluido e ajustes."
-    },
-    { 
-        id: 7, 
-        title: "Sistema de Freios", 
-        description: "Técnicas de inspeção e reparo do sistema de freios, incluindo substituição de pastilhas, discos e sangramento do sistema."
-    },
-    { 
-        id: 8, 
-        title: "Suspensão", 
-        description: "Práticas recomendadas para diagnóstico e reparo de componentes do sistema de suspensão, como amortecedores e molas."
-    },
-    { 
-        id: 9, 
-        title: "Sistema Elétrico", 
-        description: "Procedimentos de diagnóstico elétrico, incluindo testes de bateria, alternador e sistema de partida."
-    }
+    { id: 1, title: "Agile", description: "Chevrolet Agile"},
+    { id: 3, title: "Astra", description: "Chevrolet Astra"},
+    { id: 5, title: "Blazer", description: "Chevrolet Blazer"},
+    { id: 6, title: "C20", description: "Chevrolet C20"},
+    { id: 7, title: "Calibra", description: "Chevrolet Calibra"},
+    { id: 8, title: "Camaro", description: "Chevrolet Camaro"},
+    { id: 9, title: "Captiva", description: "Chevrolet Captiva"},
+    { id: 10, title: "Celta", description: "Chevrolet Celta"},
+    { id: 11, title: "Cobalt", description: "Chevrolet Cobalt"},
+    { id: 12, title: "Corsa", description: "Chevrolet Corsa"},
+    { id: 13, title: "Corsa Pick-Up", description: "Chevrolet Corsa Pick-Up"},
+    { id: 14, title: "Corsa Sedan Classic", description: "Chevrolet Corsa Classic"},
+    { id: 15, title: "Cruze", description: "Chevrolet Cruze"},
+    { id: 19, title: "Kadett", description: "Chevrolet Kadett"},
+    { id: 22, title: "Meriva", description: "Chevrolet Meriva"},
+    { id: 23, title: "Montana", description: "Chevrolet Montana"},
+    { id: 24, title: "Monza", description: "Chevrolet Monza"},
+    { id: 25, title: "Nubira", description: "Chevrolet Nubira"},
+    { id: 26, title: "Omega", description: "Chevrolet Omega"},
+    { id: 27, title: "Onix", description: "Chevrolet Onix"},
+    { id: 28, title: "Onix Plus", description: "Chevrolet Onix Plus"},
+    { id: 29, title: "Prisma", description: "Chevrolet Prisma"},
+    { id: 30, title: "S10", description: "Chevrolet S10"},
+    { id: 32, title: "Silverado", description: "Chevrolet Silverado"},
+    { id: 34, title: "Sonic", description: "Chevrolet Sonic"},
+    { id: 36, title: "Spin", description: "Chevrolet Spin"},
+    { id: 37, title: "SS10", description: "Chevrolet SS10"},
+    { id: 38, title: "Suprema", description: "Chevrolet Suprema"},
+    { id: 41, title: "Tracker", description: "Chevrolet Tracker"},
+    { id: 42, title: "Trailblazer", description: "Chevrolet Trailblazer"},
+    { id: 43, title: "Vectra", description: "Chevrolet Vectra"},
+    { id: 45, title: "Zafira", description: "Chevrolet Zafira"}
 ];
 
 export default modalItems;
