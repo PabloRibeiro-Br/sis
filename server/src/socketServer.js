40067f80bec6ee79b9380106a2a012143808d3bd
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-12-03 02:01:25 -0300

    oook

--- server/src/socketServer.js
+++ server/src/socketServer.js
@@ -5,127 +5,122 @@ const { getOpenai } = require("./ai");
 let sessions = {};
 
 const registerSocketServer = (server) => {
- const io = new Server(server, {
-   cors: {
-     origin: "*",
-     methods: ["GET", "POST"],
-   },
- });
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
- io.on("connection", (socket) => {
-   console.log(`user connected ${socket.id}`);
+  io.on("connection", (socket) => {
+    console.log(`user connected ${socket.id}`);
 
-   socket.on("session-history", (data) => {
-     sessionHistoryHandler(socket, data);
-   });
+    socket.on("session-history", (data) => {
+      sessionHistoryHandler(socket, data);
+    });
 
-   socket.on("conversation-message", (data) => {
-     conversationMessageHandler(socket, data);
-   });
+    socket.on("conversation-message", (data) => {
+      conversationMessageHandler(socket, data);
+    });
 
-   socket.on("conversation-delete", (data) => {
-     conversationDeleteHandler(socket, data);
-   });
- });
+    socket.on("conversation-delete", (data) => {
+      conversationDeleteHandler(socket, data);
+    });
+  });
 };
 
 const sessionHistoryHandler = (socket, data) => {
- const { sessionId } = data;
+  const { sessionId } = data;
 
- if (sessions[sessionId]) {
-   socket.emit("session-details", {
-     sessionId,
-     conversations: sessions[sessionId],
-   });
- } else {
-   const newSessionId = uuid();
+  if (sessions[sessionId]) {
+    // send existing session data back to user
 
-   sessions[newSessionId] = [];
+    socket.emit("session-details", {
+      sessionId,
+      conversations: sessions[sessionId],
+    });
+  } else {
+    const newSessionId = uuid();
 
-   const sessionDetails = {
-     sessionId: newSessionId,
-     conversations: [],
-   };
+    sessions[newSessionId] = [];
 
-   socket.emit("session-details", sessionDetails);
- }
+    const sessionDetails = {
+      sessionId: newSessionId,
+      conversations: [],
+    };
+
+    socket.emit("session-details", sessionDetails);
+  }
 };
 
 const conversationMessageHandler = async (socket, data) => {
- const { sessionId, message, conversationId } = data;
+  const { sessionId, message, conversationId } = data;
 
- const openai = getOpenai();
+  const openai = getOpenai();
 
- const previousConversationMessages = [];
+  const previousConversationMessages = [];
 
- if (sessions[sessionId]) {
-   const existingConversation = sessions[sessionId].find(
-     (c) => c.id === conversationId
-   );
+  if (sessions[sessionId]) {
+    const existingConversation = sessions[sessionId].find(
+      (c) => c.id === conversationId
+    );
 
-   if (existingConversation) {
-     previousConversationMessages.push(
-       ...existingConversation.messages.map((m) => ({
-         content: m.content,
-         role: m.aiMessage ? "assistant" : "user",
-       }))
-     );
-   }
+    if (existingConversation) {
+      previousConversationMessages.push(
+        ...existingConversation.messages.map((m) => ({
+          content: m.content,
+          role: m.aiMessage ? "assistant" : "user",
+        }))
+      );
+    }
 
-   const response = await openai.createChatCompletion({
-     model: "gpt-3.5-turbo",
-     messages: [
-       ...previousConversationMessages,
-       { role: "user", content: message.content },
-     ],
-     prompt: "Eu sou um assistente especializado em injeção eletrônica e motores. Como posso ajudá-lo hoje?",
-     max_tokens: 220,
-     temperature: 0.4,
-     top_p: 0.6,
-     frequency_penalty: 1.4,
-     presence_penalty: 0.2,
-     /* stop: ["\n"] -----  PARA, NÃO DEIXA A API FICAR CRIANDO TEXTOS LOGOS*/
-   });
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        ...previousConversationMessages,
+        { role: "user", content: message.content },
+      ],
+    });
 
-   const aiMessageContent = response?.data?.choices[0]?.message?.content;
+    const aiMessageContent = response?.data?.choices[0]?.message?.content;
 
-   const aiMessage = {
-     content: aiMessageContent
-       ? aiMessageContent
-       : "Error occurred when trying to get message from the AI",
-     id: uuid(),
-     aiMessage: true,
-   };
+    const aiMessage = {
+      content: aiMessageContent
+        ? aiMessageContent
+        : "Error occurred when trying to get message from the AI",
+      id: uuid(),
+      aiMessage: true,
+    };
 
-   const conversation = sessions[sessionId].find(
-     (c) => c.id === conversationId
-   );
+    const conversation = sessions[sessionId].find(
+      (c) => c.id === conversationId
+    );
 
-   if (!conversation) {
-     sessions[sessionId].push({
-       id: conversationId,
-       messages: [message, aiMessage],
-     });
-   }
+    if (!conversation) {
+      sessions[sessionId].push({
+        id: conversationId,
+        messages: [message, aiMessage],
+      });
+    }
 
-   if (conversation) {
-     conversation.messages.push(message, aiMessage);
-   }
+    if (conversation) {
+      conversation.messages.push(message, aiMessage);
+    }
 
-   const updatedConversation = sessions[sessionId].find(
-     (c) => c.id === conversationId
-   );
+    const updatedConversation = sessions[sessionId].find(
+      (c) => c.id === conversationId
+    );
 
-   socket.emit("conversation-details", updatedConversation);
- }
+    socket.emit("conversation-details", updatedConversation);
+  }
 };
 
 const conversationDeleteHandler = (_, data) => {
- const { sessionId } = data;
+  const { sessionId } = data;
 
- if (sessions[sessionId]) {
-   sessions[sessionId] = [];
- }
+  if (sessions[sessionId]) {
+    sessions[sessionId] = [];
+  }
 };
 
 module.exports = { registerSocketServer };
